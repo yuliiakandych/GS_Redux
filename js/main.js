d31ca68b3272b10b3d2cd65911706eec670d391d
yuliiakandych <y_kandych@fanshaweonline.ca>
2019-04-13 02:08:26 -0400

    h

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
 (() => {
-	 // variables first
-	 const badge = document.querySelector('img'),
-	 	   footerText =  document.querySelector('h3');
+	 // get set up withthe timline libraly
 
-	 	   function animImage(){
-	 	   	TweenMax.to(badge, 1.2, {scaleX: 1.025, scaleY: 1.025, rotation: 50, ease:Elastic.easeOut, onComplete:animFooterText});
-	 	   }
+	 let myTl = new TimelineLite();
 
-	 	   function animFooterText() {
-	 	   	TweenMax.to(footerText, 0.7, {scaleX: 2, scaleY: 2, ease:Elastic.easeInOut});
-	 	   }
-
-	 	   // animImage();
-
-	 	   badge.addEventListener('mouseover', animImage);
-
-
-
-
-
-
-
-
-
-
-
-
-
+	 myTl.to("#background", 0.5, {rotation:180, scaleX:1.05, scaleY:1.05, transformOrigin: "50% 50%"})
+	 .to("#small_type", 1, {rotation: -360, transformOrigin: "50% 50%"})
+	 .to ("h3", 1.5, {scaleX:2, scaleY:2, ease: Elastic.easeOut});
 
 
 
